15dbfa970c7774bd8a6a184e23c9df902b78284f
李承斌 <2259763904@qq.com>
2023-02-08 10:06:10 +0800

    36小结文本框表单完结

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -11,67 +11,22 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:const Text("Hero动画")
+      appBar:AppBar(
+        title:const Text("文本表单")
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-        children: _renderChild(),
-      ),
+      body:Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: (){
+                Navigator.pushNamed(context, "/page1");
+              }, 
+              child: const Text("文本表单")
+            )
+          ],
+        ),
+      )
     );
   }
-
-  List list = [
-    {
-      "image":"https://tse1-mm.cn.bing.net/th/id/OIP-C.QPH1IBosDYBqaU3O6wV3YAHaEo?pid=ImgDet&rs=1"
-    },
-    {
-      "image":"https://scpic.chinaz.net/files/pic/pic9/202009/apic27858.jpg"
-    },
-    {
-      "image":"https://pic3.zhimg.com/v2-58d652598269710fa67ec8d1c88d8f03_r.jpg?source=1940ef5c"
-    },
-    {
-      "image":"https://ts1.cn.mm.bing.net/th/id/R-C.df4462fabf18edd07195679a5f8a37e5?rik=FnNvr9jWWjHCVQ&riu=http%3a%2f%2fseopic.699pic.com%2fphoto%2f50059%2f8720.jpg_wh1200.jpg&ehk=ofb4q76uCls2S07aIlc8%2bab3H5zwrmj%2bhqiZ%2fyw3Ghw%3d&risl=&pid=ImgRaw&r=0"
-    },
-    {
-      "image":"https://pic.3gbizhi.com/2019/1112/20191112013312648.jpg"
-    },
-
-  ];
-  _renderChild(){
-    var ChildEl;
-    ChildEl = list.map((item){
-      return GestureDetector(
-        onTap:(){
-          int defaultImageIndex = 0;
-          for(int i =0;i<list.length;i++){
-            if(list[i]["image"] == item["image"]){
-              defaultImageIndex = i;
-            }
-          }
-          Navigator.pushNamed(
-            context, 
-            "/page2",
-            arguments:{
-              "image":item['image'],
-              "imageData":list,
-              "defaultImageIndex":defaultImageIndex
-            }
-          );
-        },
-        child:Container(
-          child:Hero(
-            tag:item['image'],
-            child:Image.network("${item['image']}",fit:BoxFit.cover)
-          )
-        )
-      );
-    });
-    return ChildEl.toList();
-  }
 }
-
 
