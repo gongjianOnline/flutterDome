315f1b1c5a911b871a7ae58886427ffc08c18485
李承斌 <2259763904@qq.com>
2023-02-08 13:27:19 +0800

    38小结进度条

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
-import "./routes/route.dart";
+
+
 void main(){
   runApp(MaterialApp(
-    initialRoute: "/",
-    onGenerateRoute: onGenerateRoute,
+    home:Mypage()
   ));
 }
 
+class Mypage extends StatefulWidget {
+  const Mypage({super.key});
 
+  @override
+  State<Mypage> createState() => _MypageState();
+}
+
+class _MypageState extends State<Mypage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("进度条"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          CircularProgressIndicator(),
+          SizedBox(height: 20),
+          CircularProgressIndicator(
+            backgroundColor: Colors.red,
+            valueColor:AlwaysStoppedAnimation(Colors.black)
+          ),
+          SizedBox(height: 20),
+          LinearProgressIndicator(),
+          SizedBox(height: 20),
+          LinearProgressIndicator(
+            backgroundColor: Colors.white,
+            valueColor: AlwaysStoppedAnimation(Colors.blue),
+            value:0.52
+          )
+        ],
+      ),
+    );
+  }
+}
 
---
+++ lib/main01.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import "./routes/route.dart";
+void main(){
+  runApp(MaterialApp(
+    initialRoute: "/",
+    onGenerateRoute: onGenerateRoute,
+  ));
+}
+
+
+
