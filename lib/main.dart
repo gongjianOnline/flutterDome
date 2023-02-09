9579c84f515c3cd9b8875fc34c880f321b9953d0
李承斌 <2259763904@qq.com>
2023-02-09 14:21:38 +0800

    40小结时间组件完结

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,75 @@
 import 'package:flutter/material.dart';
- 
-void main() => runApp(MyApp());
+import 'package:flutter_localizations/flutter_localizations.dart';
+void main(){
+  runApp(const MaterialApp(
+    home:MyApp(),
+    localizationsDelegates: [
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+    ],
+    supportedLocales:[
+      //此处
+      Locale('zh', 'CH'),
+      Locale('en', 'US'),
+    ],
 
-class MyApp extends StatelessWidget {
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TimeOfDay time = TimeOfDay.now();
+  DateTime dateTime = DateTime.now();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("flutter"),
-        ),
-        body: Stack(
+    print(dateTime);
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text("时间格式")
+      ),
+      body:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Positioned(child: Text("1+1")),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: GridView.count(
-                shrinkWrap: true,
-                crossAxisCount: 3,
-                children: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                    .map((val) => TextButton(
-                        onPressed: () {}, child: Text(val.toString())))
-                    .toList(),
-              ),
+            InkWell(
+              child: Text("${time.hour}:${time.minute}"),
+              onTap: ()async{
+                TimeOfDay? newTime = await showTimePicker(
+                  context: context, 
+                  initialTime: time
+                );
+                setState(() {
+                  time = newTime!;
+                });
+              },
+            ),
+            SizedBox(height: 30),
+            InkWell(
+              child: Text("${dateTime.year}-${dateTime.month}-${dateTime.day}"),
+              onTap: ()async{
+                DateTime? newDateTime = await showDatePicker(
+                  context: context, 
+                  initialDate: dateTime, 
+                  firstDate: DateTime(1990,1,1), 
+                  lastDate: DateTime(2050,1,1)
+                );
+                setState(() {
+                  dateTime = newDateTime!;
+                });
+              },
             )
           ],
         ),
-      ),
+      )
     );
   }
 }
+
+
+
