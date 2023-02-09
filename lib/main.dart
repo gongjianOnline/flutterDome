d184f759121b3b6e3c4fcb788219c673b3dee3de
李承斌 <2259763904@qq.com>
2023-02-09 08:42:50 +0800

    备份

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,185 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import "dart:async";
 
-void main(){
-  runApp(const MaterialApp(
-    home:MyApp()
-  ));
+void main() {
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final _controller = StreamController.broadcast();
-
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title:const Text("Stream流操作")),
-      body:Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: (){
-                _controller.add(1);
-              }, 
-              child:const Text("add1")
-            ),
-            ElevatedButton(
-              onPressed: (){
-                _controller.add(2);
-              }, 
-              child:const Text("add2")
-            ),
-            ElevatedButton(
-              onPressed: (){
-                _controller.add(3);
-              }, 
-              child:const Text("add2")
-            ),
-            StreamBuilder(
-              stream:_controller.stream,
-              builder: (context,snapshot){
-                return Text("${snapshot.data}");
-              }
-            ),
-            StreamBuilder(
-              stream: _controller.stream.where((event) => event>2),
-              builder: (context,snapshot){
-                return  Text("${snapshot.data}");
-              },
-            )
-
-          ],
-        ),
-      )
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
     );
   }
 }
 
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final StreamController<int> _inputController =
+      StreamController.broadcast(); //可以有多个监听者
+  final StreamController<int> _scoreController =
+      StreamController.broadcast(); 
+
+  int score=0;
+      
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: StreamBuilder(
+            stream: _scoreController.stream,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                score+=snapshot.data as int;
+                return Text("得分：${score}");
+              }
+              if (snapshot.hasError) {
+                return Text("Error:${snapshot.error}");
+              }
+              return const Text("监听中..");
+            }),
+      ),
+      body: Stack(
+        children: [
+          ...List.generate(3, (index) {
+            return Game(
+              inputController: _inputController,
+              scoreController: _scoreController,
+            );
+          }),
+          KeyPad(
+            inputController: _inputController,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+//算数  游戏 子组件
+class Game extends StatefulWidget {
+  final StreamController<int> inputController;
+   final StreamController<int> scoreController;
+  
+  const Game({super.key, required this.inputController,required this.scoreController});
+
+  @override
+  State<Game> createState() => _GameState();
+}
+
+class _GameState extends State<Game> with SingleTickerProviderStateMixin {
+  late int a, b;
+  late double x;
+  late AnimationController _animationController;
+  late Color color;
+  @override
+  void reset() {
+    a = Random().nextInt(5) + 1;
+    b = Random().nextInt(5);
+    x = Random().nextDouble() * 320;
+    color = Colors.primaries[Random().nextInt(Colors.primaries.length)];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    reset();
+    _animationController = AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: Random().nextInt(5000) + 5000));
+
+    _animationController.forward(); //执行一次动画
+
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        //监听动画执行完成
+        //生成随机数
+        reset();
+         widget.scoreController.add(-1);  //最底部减去1分
+        _animationController.forward(from: 0.0); //重新执行一次动画
+      }
+    });
+
+    //监听键盘按键
+    widget.inputController.stream.listen((event) {
+      print(event);
+      if (a + b == event) {
+        reset();
+        widget.scoreController.add(3);  //打中加3分
+        _animationController.forward(from: 0.0);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, child) {
+          return Positioned(
+            top: Tween(begin: -50.0, end: 560.0)
+                .animate(_animationController)
+                .value,
+            left: x,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(8, 5, 8, 5),
+              decoration: BoxDecoration(
+                  color: color, borderRadius: BorderRadius.circular(18)),
+              child: Text("$a+$b=？", style: TextStyle(fontSize: 24)),
+            ),
+          );
+        });
+  }
+}
+
+//键盘 子组件
+class KeyPad extends StatelessWidget {
+  final StreamController<int> inputController;
+  const KeyPad({super.key, required this.inputController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        color: Colors.red,
+        child: GridView.count(
+          shrinkWrap: true, //收缩
+          crossAxisCount: 3,
+          childAspectRatio: 5 / 2,
+          children: List.generate(9, (index) {
+            return TextButton(
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                        const RoundedRectangleBorder()),
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.primaries[index][300]),
+                    foregroundColor: MaterialStateProperty.all(Colors.black)),
+                onPressed: () {
+                  inputController.add(index + 1);
+                },
+                child: Text("${index + 1}",
+                    style: Theme.of(context).textTheme.headline4));
+          }),
+        ),
+      ),
+    );
+  }
+}
