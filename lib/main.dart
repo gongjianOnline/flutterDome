1751c3e8534c0b860566d09403b289197d6e2aa4
李承斌 <2259763904@qq.com>
2023-02-09 10:46:23 +0800

    39小结计算小游戏练习

--- lib/main.dart
+++ lib/main.dart
@@ -1,183 +1,30 @@
-import 'dart:async';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
+ 
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final StreamController<int> _inputController =
-      StreamController.broadcast(); //可以有多个监听者
-  final StreamController<int> _scoreController =
-      StreamController.broadcast(); 
-
-  int score=0;
-      
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: StreamBuilder(
-            stream: _scoreController.stream,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                score+=snapshot.data as int;
-                return Text("得分：${score}");
-              }
-              if (snapshot.hasError) {
-                return Text("Error:${snapshot.error}");
-              }
-              return const Text("监听中..");
-            }),
-      ),
-      body: Stack(
-        children: [
-          ...List.generate(3, (index) {
-            return Game(
-              inputController: _inputController,
-              scoreController: _scoreController,
-            );
-          }),
-          KeyPad(
-            inputController: _inputController,
-          )
-        ],
-      ),
-    );
-  }
-}
-
-//算数  游戏 子组件
-class Game extends StatefulWidget {
-  final StreamController<int> inputController;
-   final StreamController<int> scoreController;
-  
-  const Game({super.key, required this.inputController,required this.scoreController});
-
-  @override
-  State<Game> createState() => _GameState();
-}
-
-class _GameState extends State<Game> with SingleTickerProviderStateMixin {
-  late int a, b;
-  late double x;
-  late AnimationController _animationController;
-  late Color color;
-  @override
-  void reset() {
-    a = Random().nextInt(5) + 1;
-    b = Random().nextInt(5);
-    x = Random().nextDouble() * 320;
-    color = Colors.primaries[Random().nextInt(Colors.primaries.length)];
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    reset();
-    _animationController = AnimationController(
-        vsync: this,
-        duration: Duration(milliseconds: Random().nextInt(5000) + 5000));
-
-    _animationController.forward(); //执行一次动画
-
-    _animationController.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        //监听动画执行完成
-        //生成随机数
-        reset();
-         widget.scoreController.add(-1);  //最底部减去1分
-        _animationController.forward(from: 0.0); //重新执行一次动画
-      }
-    });
-
-    //监听键盘按键
-    widget.inputController.stream.listen((event) {
-      print(event);
-      if (a + b == event) {
-        reset();
-        widget.scoreController.add(3);  //打中加3分
-        _animationController.forward(from: 0.0);
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-        animation: _animationController,
-        builder: (context, child) {
-          return Positioned(
-            top: Tween(begin: -50.0, end: 560.0)
-                .animate(_animationController)
-                .value,
-            left: x,
-            child: Container(
-              padding: const EdgeInsets.fromLTRB(8, 5, 8, 5),
-              decoration: BoxDecoration(
-                  color: color, borderRadius: BorderRadius.circular(18)),
-              child: Text("$a+$b=？", style: TextStyle(fontSize: 24)),
-            ),
-          );
-        });
-  }
-}
-
-//键盘 子组件
-class KeyPad extends StatelessWidget {
-  final StreamController<int> inputController;
-  const KeyPad({super.key, required this.inputController});
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        color: Colors.red,
-        child: GridView.count(
-          shrinkWrap: true, //收缩
-          crossAxisCount: 3,
-          childAspectRatio: 5 / 2,
-          children: List.generate(9, (index) {
-            return TextButton(
-                style: ButtonStyle(
-                    shape: MaterialStateProperty.all(
-                        const RoundedRectangleBorder()),
-                    backgroundColor:
-                        MaterialStateProperty.all(Colors.primaries[index][300]),
-                    foregroundColor: MaterialStateProperty.all(Colors.black)),
-                onPressed: () {
-                  inputController.add(index + 1);
-                },
-                child: Text("${index + 1}",
-                    style: Theme.of(context).textTheme.headline4));
-          }),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("flutter"),
+        ),
+        body: Stack(
+          children: [
+            Positioned(child: Text("1+1")),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: GridView.count(
+                shrinkWrap: true,
+                crossAxisCount: 3,
+                children: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                    .map((val) => TextButton(
+                        onPressed: () {}, child: Text(val.toString())))
+                    .toList(),
+              ),
+            )
+          ],
         ),
       ),
     );
