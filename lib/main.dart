95690b01374ffce700e2f7d87291d9d6205918e2
龚箭 <123456@163.com>
2023-02-09 21:11:26 +0800

    42小结下拉刷新完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:dio/dio.dart';
+import "dart:convert";
 void main(){
   runApp(MaterialApp(
     home:MyApp()
@@ -14,6 +15,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List listData = [];
+  int index = 1;
+  @override
+  void initState() {
+    super.initState();
+     _getData();
+    // print("1111111");
+    // print(res);
+  }
+
+  _getData()async{
+    var response = await Dio().get("https://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=$index");
+    var result=json.decode(response.data);
+    setState(() {
+      listData = result['result'];
+      index++;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,10 +43,13 @@ class _MyAppState extends State<MyApp> {
       ),
       body:RefreshIndicator(
         child: ListView(
-          children: [],
+          children: listData.map((item){
+            return ListTile(title:Text("${item['title']}"));
+          }).toList(),
         ), 
         onRefresh:()async{
           print("底部更新");
+          _getData();
         }
       )
     );
