d81987ed8ca6858905929f0797faaf0b317cadc8
李承斌 <2259763904@qq.com>
2023-02-13 10:32:45 +0800

    47小结播放视频组件完结

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,21 +10,63 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late VideoPlayerController videoPlayerController = VideoPlayerController.network(
+    'https://vfx.mtime.cn/Video/2019/02/04/mp4/190204084208765161.mp4');
+  
+  late ChewieController chewieController = ChewieController(
+    aspectRatio:3/2,
+    videoPlayerController: videoPlayerController,
+    autoPlay: true,
+    looping: true,
+    optionsBuilder: (context, defaultOptions)async{
+      await showModalBottomSheet(
+        context: context, 
+        builder: (context){
+          return ListView(
+            children: [
+              ListTile(
+                title:const Text("播放速度"),
+                onTap: (){
+                  print("-----------");
+                  print(defaultOptions);
+                  defaultOptions[0].onTap!();
+                },
+              ),
+              ListTile(
+                title:const Text("取消"),
+                onTap: (){
+
+                },
+              )
+            ],
+          );
+        }
+      );
+    }
+  );
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+    chewieController.dispose();
+    super.dispose();
+  }
+
+  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title:const Text("打开内部APP")
+      appBar: AppBar(
+        title:const Text("视频播放组件")
       ),
-      body:ElevatedButton(
-        onPressed: () async {
-          final Uri weixin = Uri.parse('weixin://');
-          if(await canLaunchUrl(weixin)){
-            await launchUrl(weixin);
-          }
-        },
-        child:const Text("开微信"),
-      )
+      body: AspectRatio(
+        aspectRatio:3/2,
+        child:Chewie(
+          controller: chewieController,
+        )
+      ),
     );
   }
 }
+
