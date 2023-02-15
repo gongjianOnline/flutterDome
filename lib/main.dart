5819682910c66bee7a37516f9bc8ff0c344dc423
李承斌 <2259763904@qq.com>
2023-02-15 10:18:02 +0800

    55.小结完成软件更新和自动升级功能

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
-import 'dart:async';
 import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:amap_flutter_location/amap_flutter_location.dart';
-import 'package:amap_flutter_location/amap_location_option.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:dio/dio.dart';
+import 'package:open_filex/open_filex.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home:MyApp()
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -17,149 +21,75 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // 初始化参数
-  late Map<String, Object> _locationResult;
-  late StreamSubscription<Map<String, Object>> _locationListener;
-  AMapFlutterLocation _locationPlugin = new AMapFlutterLocation();
-  
-  // 初始化
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    AMapFlutterLocation.updatePrivacyShow(true, true);
-    AMapFlutterLocation.updatePrivacyAgree(true);
-    // 申请定位权限
-    requestPermission();
-    // 初始化Key
-    AMapFlutterLocation.setApiKey(
-        "019bb1dd550a1d94aeae3e1557bcd365", "ios ApiKey"
-    );
-    // 注册定位结果监听
-    _locationListener = _locationPlugin
-      .onLocationChanged()
-      .listen((event) {
-        print("------------");
-        print(event);
-        setState(() {
-          _locationResult = event;
-        });
-      });
+    _getAppInfo();
+    _getDocDirectory();
   }
 
-  // 生命周期函数销毁
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    ///移除定位监听
-    if (null != _locationListener) {
-      _locationListener.cancel();
-    }
-
-    ///销毁定位
-    if (null != _locationPlugin) {
-      _locationPlugin.destroy();
-    }
-
+  // 获取应用信息
+  _getAppInfo()async{
+    // 获取应用信息
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    print(packageInfo.version);
   }
+  // 获取存储路径
+  _getDocDirectory()async{
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String appDocPath = appDocDir.path;
+    print("-------");
+    print(appDocPath);
+  }
+  // 检查权限
+  _checkPermission()async{
+    if(Theme.of(context).platform == TargetPlatform.android){
+      final status = await Permission.storage.status;
+      if(status != PermissionStatus.granted){
+        final result = await Permission.storage.request();
+        if (result == PermissionStatus.granted) {
+          return true;
+        }
+      }else{
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // 下载文件
+  _downLoad()async{
+    var flag = await _checkPermission();
+    if(flag){
+      final directory = await getExternalStorageDirectory();
+      print("获取本地存储路径");
+      print(directory);
+      String localPath  = directory!.path;
+      String appName = "aaa.apk";
+      String savePath="$localPath/$appName";
+      String ajaxUrl = 'https://jd.itying.com/jdshop.apk';
+      Dio dio = Dio();
+      await dio.download(ajaxUrl, savePath);
+      await OpenFilex.open(savePath,type: "application/vnd.android.package-archive");
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title:const Text('高德地图')
-        ),
-        body:Center(
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: (){
-                  _startLocation();
-                }, 
-                child: const Text("开启定位")
-              )
-            ],
-          ),
-        )
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text("自动更新组件")
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          _downLoad();
+        },
+        child:const Icon(Icons.add),
       ),
     );
   }
-
-  // 申请定位权限
-  requestPermission()async{
-    bool hasLocationPermission = await requestLocationPermission();
-    if (hasLocationPermission) {
-      print("定位权限申请通过");
-    } else {
-      print("定位权限申请不通过");
-    }
-  }
-  // 同意true 不同意返回false 如果是false 会重复提示一次是否允许定位权限
-  requestLocationPermission()async{
-    var status = await Permission.location.status;
-    if (status == PermissionStatus.granted) {
-      //已经授权
-      return true;
-    } else {
-      //未授权则发起一次申请
-      status = await Permission.location.request();
-      if (status == PermissionStatus.granted) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
-
-   ///设置定位参数
-  void _setLocationOption() {
-    if (null != _locationPlugin) {
-      AMapLocationOption locationOption = new AMapLocationOption();
-      ///是否单次定位
-      locationOption.onceLocation = false;
-      ///是否需要返回逆地理信息
-      locationOption.needAddress = true;
-      ///逆地理信息的语言类型
-      locationOption.geoLanguage = GeoLanguage.DEFAULT;
-      locationOption.desiredLocationAccuracyAuthorizationMode =
-          AMapLocationAccuracyAuthorizationMode.ReduceAccuracy;
-      locationOption.fullAccuracyPurposeKey = "AMapLocationScene";
-      ///设置Android端连续定位的定位间隔
-      locationOption.locationInterval = 2000;
-      ///设置Android端的定位模式<br>
-      ///可选值：<br>
-      ///<li>[AMapLocationMode.Battery_Saving]</li>
-      ///<li>[AMapLocationMode.Device_Sensors]</li>
-      ///<li>[AMapLocationMode.Hight_Accuracy]</li>
-      locationOption.locationMode = AMapLocationMode.Hight_Accuracy;
-      ///设置iOS端的定位最小更新距离<br>
-      locationOption.distanceFilter = -1;
-      ///设置iOS端期望的定位精度
-      /// 可选值：<br>
-      /// <li>[DesiredAccuracy.Best] 最高精度</li>
-      /// <li>[DesiredAccuracy.BestForNavigation] 适用于导航场景的高精度 </li>
-      /// <li>[DesiredAccuracy.NearestTenMeters] 10米 </li>
-      /// <li>[DesiredAccuracy.Kilometer] 1000米</li>
-      /// <li>[DesiredAccuracy.ThreeKilometers] 3000米</li>
-      locationOption.desiredAccuracy = DesiredAccuracy.Best;
-      ///设置iOS端是否允许系统暂停定位
-      locationOption.pausesLocationUpdatesAutomatically = false;
-      ///将定位参数设置给定位插件
-      _locationPlugin.setLocationOption(locationOption);
-    }
-  }
-
-  // 开始定位
-  _startLocation(){
-    if(null != _locationPlugin){
-      _setLocationOption();
-      _locationPlugin.startLocation();
-    }
-  }
-  
-
-
-  
 }
+
